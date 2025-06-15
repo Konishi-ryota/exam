f60973d866d9d0b58beecd1e45b41518ff40163d
ryotakonishi <ryota.konishi@icloud.com>
2025-06-16 03:37:47 +0900

    [修正]変数追加と削除

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int bulletSpeed;
-    public int PistlebulletAttack;
-    public int ARbulletAttack;
-    public int SMGbulletAttack;
-    public int SRbulletAttack;
+    public int bulletAttack;
     public float interval = 1f;
 
     private Hero _hero;
-    private int _index;
 
     // Start is called before the first frame update
     void Start()
     {
         _hero = FindObjectOfType<Hero>();
-        _index = _hero.playerIndex;
     }
 
     // Update is called once per frame
