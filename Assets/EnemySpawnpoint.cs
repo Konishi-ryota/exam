6857c63e2b6b744f682d73450158876c15a2a853
ryotakonishi <ryota.konishi@icloud.com>
2025-06-17 18:58:00 +0900

    [remove]いらないコード削除

--- Assets/EnemySpawnpoint.cs
+++ Assets/EnemySpawnpoint.cs
@@ -20,7 +20,6 @@ public class EnemySpawnpoint : MonoBehaviour
         int rnd = 0;
         if (Time.frameCount % 60 == 0 && _isSpawning)
         {
-            Hero _hero = FindAnyObjectByType<Hero>();
             rnd = Random.Range(0, 11);
             Instantiate(enemy, spawnpoint.transform.position, Quaternion.identity);
         }
