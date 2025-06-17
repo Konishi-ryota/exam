fa8189f562dcc9be4360171735e30e6df4d4391c
ryotakonishi <ryota.konishi@icloud.com>
2025-06-17 18:25:23 +0900

    [add]ショップを開いてるときに敵が生成されないように付け足し

--- Assets/EnemySpawnpoint.cs
+++ Assets/EnemySpawnpoint.cs
@@ -7,6 +7,7 @@ public class EnemySpawnpoint : MonoBehaviour
     [SerializeField] GameObject spawnpoint;
     [SerializeField] GameObject enemy;
     private Hero _hero;
+    private bool _isSpawning = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +18,19 @@ public class EnemySpawnpoint : MonoBehaviour
     void Update()
     {
         int rnd = 0;
-        if (Time.frameCount % 60 == 0)
+        if (Time.frameCount % 60 == 0 && _isSpawning)
         {
             Hero _hero = FindAnyObjectByType<Hero>();
             rnd = Random.Range(0, 11);
             Instantiate(enemy, spawnpoint.transform.position, Quaternion.identity);
+        }
+        if (_hero._RemainTime <= 0)
+        {
+            _isSpawning = false;
+        }
+        else
+        {
+            _isSpawning=true;
         }
     }
 }
