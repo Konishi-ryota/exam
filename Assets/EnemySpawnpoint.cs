ddadb490208ba68745f1f16fa49eac8963725487
ryotakonishi <ryota.konishi@icloud.com>
2025-06-17 18:03:41 +0900

    [add]スポーンポイントスクリプト追加

---
+++ Assets/EnemySpawnpoint.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnpoint : MonoBehaviour
+{
+    [SerializeField] GameObject spawnpoint;
+    [SerializeField] GameObject enemy;
+    private Hero _hero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hero _hero = FindAnyObjectByType<Hero>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rnd = 0;
+        if (Time.frameCount % 60 == 0)
+        {
+            Hero _hero = FindAnyObjectByType<Hero>();
+            rnd = Random.Range(0, 11);
+            Instantiate(enemy, spawnpoint.transform.position, Quaternion.identity);
+            enemy.SetActive(true);
+        }
+        //if (_hero._RemainTime < 0)
+        //{
+        //    Destroy(enemy);
+        //}
+    }
+}
