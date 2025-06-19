028527e8967bdd28ea3abec518e6ac74d2d6945a
ryotakonishi <ryota.konishi@icloud.com>
2025-06-20 02:40:49 +0900

    [remove]いらないもの色々削除

--- Assets/EnemySpawnpoint.cs
+++ Assets/EnemySpawnpoint.cs
@@ -7,23 +7,17 @@ public class EnemySpawnpoint : MonoBehaviour
 {
     [SerializeField] GameObject[] spawnpoint;
     [SerializeField] GameObject[] enemy;
-    [SerializeField] Text timerText;
 
-    private Hero _hero;
     private bool _isSpawning = true;
-    private float _timer = 0;
-    [NonSerialized] public int _remainTime;
-    public int _stageTimer = 30;
     // Start is called before the first frame update
     void Start()
     {
-        _hero = FindAnyObjectByType<Hero>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        WaveSetting();
         int rnd = 0;
         if (Time.frameCount % Application.targetFrameRate == 0 && _isSpawning)
         {
@@ -37,29 +31,6 @@ public class EnemySpawnpoint : MonoBehaviour
         else
         {
             _isSpawning= true;
-        }
-    }
-
-    /// <summary>
-    /// ウェーブ切り替え機能
-    /// </summary>
-    private void WaveSetting()
-    {
-        _timer += Time.deltaTime;
-        _remainTime = _stageTimer - (int)_timer;
-        timerText.text = "残り時間 " + _remainTime.ToString("D2") + " 秒";
-        if (_remainTime <= 0)
-        {
-            _hero._HouseEnter = true;
-            _timer = _stageTimer;//Sを押すまでは残り時間を0秒にする
-            Time.timeScale = 0;
-            timerText.text = "Sを押してスタート";
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                _hero._HouseEnter = false;
-                _timer = 0;//Sを押したらタイマーを元通りに戻す
-                Time.timeScale = 1;
-            }
         }
     }
 }
