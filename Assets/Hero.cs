a8421f4dc2fe5ae7135cfa90a0aeffd49bfb6ece
ryotakonishi <ryota.konishi@icloud.com>
2025-06-20 02:41:14 +0900

    [add]ウェーブ切り替え移動

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -12,6 +12,7 @@ public class Hero : MonoBehaviour
     [SerializeField] GameObject[] WarningUI;
     [SerializeField] GameObject PauseUI;
     [SerializeField] GameObject GameOverUI;
+    [SerializeField] Text timerText;
     [SerializeField] GameObject[] WeaponGameSceneUI;
 
     [SerializeField,Header("上からピストル、AR、SMG、SR")] int[] WeaponGold;
@@ -33,14 +34,13 @@ public class Hero : MonoBehaviour
     private int _SRcount = 0;
     [NonSerialized] public int _PlayerIndex = 0;
     private Rigidbody2D _rig = null;
-    private int _Level;
-    private int _HeroExp;
-    private float _PistleTimer;
-    private float _ARTimer;  
-    private float _SMGTimer;
-    private float _SRTimer;
+    [SerializeField] float[] WeaponTimer; 
+    private float _timer = 0;
 
     [NonSerialized] public bool _HouseEnter;
+    [NonSerialized] public int _remainTime;
+    public int _stageTimer = 30;
+    private int waveCount = 1;
     private Enemy _enemy;
     private Bullet _bullet;
     #endregion
@@ -52,10 +52,10 @@ public class Hero : MonoBehaviour
         _rig = GetComponent<Rigidbody2D>();
         _enemy =FindAnyObjectByType<Enemy>();
         _bullet = FindAnyObjectByType<Bullet>();
-        _PistleTimer =PistleInterval;
-        _ARTimer = ARInterval;
-        _SMGTimer = SMGInterval;
-        _SMGTimer = SRInterval;
+        WeaponTimer[0] =PistleInterval;
+        WeaponTimer[1] = ARInterval;
+        WeaponTimer[2] = SMGInterval;
+        WeaponTimer[3] = SRInterval;
         //H_exp = enemy.E_exp;
         //H_Gold = enemy.E_Gold;  
     }
@@ -128,11 +128,6 @@ public class Hero : MonoBehaviour
             PauseUI.SetActive(false);
             Time.timeScale = 1;
         }
-        if (_HeroExp > 100)
-        {
-            _HeroExp = _HeroExp - 100;
-           _Level =_Level + 1;
-        }
         if (H_hp <= 0)
         {
             Destroy(this.gameObject);
@@ -144,34 +139,57 @@ public class Hero : MonoBehaviour
         Time.timeScale = 0;
     }
     /// <summary>
+    /// ウェーブ切り替え機能
+    /// </summary>
+    private void WaveSetting()
+    {
+        _timer += Time.deltaTime;
+        _remainTime = _stageTimer - (int)_timer;
+        timerText.text = "残り時間 " + _remainTime.ToString("D2") + " 秒";
+        if (_remainTime <= 0)
+        {
+            _HouseEnter = true;
+            _timer = _stageTimer;//Sを押すまでは残り時間を0秒にする
+            Time.timeScale = 0;
+            timerText.text = "Sを押してスタート";
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                _HouseEnter = false;
+                _timer = 0;//Sを押したらタイマーを元通りに戻す
+                waveCount++;
+                Time.timeScale = 1;
+            }
+        }
+    }
+    /// <summary>
     /// 弾を撃つ時に呼ばれるメソッド
     /// </summary>
     public void bulletshot()
     {
-        if (_PlayerIndex == 0 && _PistleTimer < Time.time)//delta.timeを足し続けるよりもtimeで必要な時だけ呼び出した方が軽い
+        if (_PlayerIndex == 0 && WeaponTimer[0] < Time.time)//delta.timeを足し続けるよりもtimeで必要な時だけ呼び出した方が軽い
         {
             Instantiate(Bullet[0],muzzle.transform.position,Quaternion.identity);
             Bullet[0].SetActive(true);
-            _PistleTimer = Time.time + PistleInterval;//if文が呼ばれたら、timeにインターバルを足してピストルタイマーに代入
+            WeaponTimer[0] = Time.time + PistleInterval;//if文が呼ばれたら、timeにインターバルを足してピストルタイマーに代入
             　　　　　　　　　　　　　　　　　　　　　//一時的にtimeよりもタイマーの方がインターバル時間分大きくなるため、ちゃんと機能する
         }
-        if (_PlayerIndex == 1 && _ARTimer < Time.time && _ARcount > 0)
+        if (_PlayerIndex == 1 && WeaponTimer[1] < Time.time && _ARcount > 0)
         {
             Instantiate(Bullet[1], muzzle.transform.position, Quaternion.identity);
             Bullet[1].SetActive(true);
-            _ARTimer = Time.time + ARInterval;
+            WeaponTimer[1] = Time.time + ARInterval;
         }
-        if (_PlayerIndex == 2 && _SMGTimer < Time.time && _SMGcount > 0)
+        if (_PlayerIndex == 2 && WeaponTimer[2] < Time.time && _SMGcount > 0)
         {
             Instantiate(Bullet[2], muzzle.transform.position, Quaternion.identity);
             Bullet[2].SetActive(true);
-            _SMGTimer = Time.time + SMGInterval;
+            WeaponTimer[2] = Time.time + SMGInterval;
         }
-        if (_PlayerIndex == 3 && _SRTimer < Time.time && _SRcount > 0)
+        if (_PlayerIndex == 3 && WeaponTimer[3] < Time.time && _SRcount > 0)
         {
             Instantiate(Bullet[3], muzzle.transform.position, Quaternion.identity);
             Bullet[3].SetActive(true);
-            _SRTimer = Time.time + SRInterval;
+            WeaponTimer[3] = Time.time + SRInterval;
         }
     }
     /// <summary>
