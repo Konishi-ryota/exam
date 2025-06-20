c33f34a71eb74a444e4ef0aed0f70d1c41ea6c6c
ryotakonishi <ryota.konishi@icloud.com>
2025-06-20 13:48:18 +0900

    [add]ウェーブ切り替え機能スポーン用スクリプトに移動

--- Assets/EnemySpawnpoint.cs
+++ Assets/EnemySpawnpoint.cs
@@ -11,15 +11,22 @@ public class EnemySpawnpoint : MonoBehaviour
 
     private bool _isSpawning = true;
     private int _spawnrnd;
+    private float _timer = 0;
+    [NonSerialized] public int _remainTime;
+    private int waveCount = 1;
+    public int _stageTimer = 30;
+    [SerializeField] Text timerText;
+    private Hero _hero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _hero = FindAnyObjectByType<Hero>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        WaveSetting();
         if (Time.frameCount % Application.targetFrameRate == 0 && _isSpawning)
         {
             EnemySpawn();
@@ -40,6 +47,29 @@ public class EnemySpawnpoint : MonoBehaviour
         {
         Instantiate(enemy[0], spawnpoint[0].transform.position, Quaternion.identity);
 
+        }
+    }
+    /// <summary>
+    /// ウェーブ切り替え機能
+    /// </summary>
+    private void WaveSetting()
+    {
+        _timer += Time.deltaTime;
+        _remainTime = _stageTimer - (int)_timer;
+        timerText.text = "残り時間 " + _remainTime.ToString("D2") + " 秒";
+        if (_remainTime <= 0)
+        {
+            _hero._HouseEnter = true;
+            _timer = _stageTimer;//Sを押すまでは残り時間を0秒にする
+            Time.timeScale = 0;
+            timerText.text = "Sを押してスタート";
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                _hero._HouseEnter = false;
+                _timer = 0;//Sを押したらタイマーを元通りに戻す
+                waveCount++;
+                Time.timeScale = 1;
+            }
         }
     }
 }
