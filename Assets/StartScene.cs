f5a4383c7b7a33716923825a7c226be55025415a
ryotakonishi <ryota.konishi@icloud.com>
2025-06-23 02:47:50 +0900

    [add]スタートシーン更新

---
+++ Assets/StartScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScene : MonoBehaviour
+{
+    [SerializeField] Text GameStartUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(GameScene());
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    IEnumerator GameScene()
+    {
+        yield return new WaitForSeconds(3);
+    }
+}
