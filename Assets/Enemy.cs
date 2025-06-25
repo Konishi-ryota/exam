1f287a066184f109a261139fb6a6b84cbd1c7d97
ryotakonishi <ryota.konishi@icloud.com>
2025-06-26 03:24:23 +0900

    [add]コメントアウト追加

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -19,29 +19,25 @@ public class Enemy : MonoBehaviour
         _rig = GetComponent<Rigidbody2D>();
         Application.targetFrameRate = 60;
 
-        _hero = FindAnyObjectByType<Hero>();
+        _hero = FindAnyObjectByType<Hero>();//HeroからHeroという名前のスクリプトを取ってくる
         _spawn = FindAnyObjectByType<EnemySpawnpoint>();
-        if (!_hero)
-        {
-            Debug.LogWarning("hero is null");
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rig.velocity = new Vector3(E_speed, 0);
-        if (_spawn._remainTime <= 0)
+        _rig.velocity = new Vector3(E_speed, 0);//移動
+        if (_spawn._remainTime <= 0)//ウェーブ残り時間が0秒未満の時
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject);//破壊する
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "bullet")
+        if (collision.gameObject.tag == "bullet")//弾に当たったら
         {
-            _bullet = FindAnyObjectByType<Bullet>();
-            E_hp = E_hp - _bullet.bulletAttackPower;
+            _bullet = FindAnyObjectByType<Bullet>();//弾のスクリプトの情報をとってくる
+            E_hp = E_hp - _bullet.bulletAttackPower;//HPを弾の攻撃力分減らす
             Destroy(collision.gameObject); //弾丸消す
             EnemyDeath();
         }
@@ -53,17 +49,20 @@ public class Enemy : MonoBehaviour
         }
         if (collision.gameObject.tag == "Player")
         {
-            _hero.H_HP -= E_attack;
-            E_hp -=E_hp;
+            _hero.H_HP -= E_attack;//HeroのHPを攻撃力文減らす
+            E_hp -= E_hp;//HPを0にする
             Debug.Log($"remain {_hero.H_HP} player health");
             _hero.SetHP();
             EnemyDeath();
         }
-        if (collision.gameObject.tag == "Respawn")
+        if (collision.gameObject.tag == "Respawn")//家に触れたら
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject);//破壊する
         }
     }
+    /// <summary>
+    /// 敵が死んだときの処理
+    /// </summary>
     private void EnemyDeath()
     {
         if (E_hp <= 0)
@@ -74,7 +73,7 @@ public class Enemy : MonoBehaviour
         }
         else
         {
-            Debug.Log($"remain {gameObject.name} health : {E_hp}");
+            Debug.Log($"remain {gameObject.name} health : {E_hp}");//残りHP表示
         }
     }
 }
