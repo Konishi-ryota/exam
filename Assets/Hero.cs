a3694307047a4a4a758364cf109c4cb8f5f3fe98
ryotakonishi <ryota.konishi@icloud.com>
2025-06-26 03:29:07 +0900

    [add]コメント追加

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Hero : MonoBehaviour
+    //God Classすぎる、反省
 {
     #region　変数
     [SerializeField] GameObject muzzle;
 
     [SerializeField] AudioClip[] ShotSE;
 
-    [SerializeField] GameObject[] ShopUI;
+    [SerializeField] GameObject ShopUI;
     [SerializeField] GameObject[] WarningUI;
     [SerializeField] Text GoldText;
     [SerializeField] Text HPText;
@@ -24,7 +25,7 @@ public class Hero : MonoBehaviour
     [SerializeField] GameObject[] WeaponSelectUI;
     [SerializeField] GameObject[] Bullet;
     [Header("上からBolt、Spark、Waveform、Pulse")]
-    [SerializeField] GameObject[] weaponList;
+    [SerializeField] GameObject[] bulletList;
 
     [Header("プレイヤー設定")]
     public int H_HP;
@@ -38,7 +39,7 @@ public class Hero : MonoBehaviour
     private int _ARcount = 0;
     private int _SMGcount = 0;
     private int _SRcount = 0;
-    [NonSerialized] public int _PlayerIndex = 0;
+    [NonSerialized] public int _bulletIndex = 0;
     private float _PistleTimer;
     private float _ARTimer;  
     private float _SMGTimer;
@@ -54,7 +55,6 @@ public class Hero : MonoBehaviour
     public bool _isDead;
     #endregion
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -63,8 +63,8 @@ public class Hero : MonoBehaviour
         _audio = GetComponent<AudioSource>();
         _house = FindAnyObjectByType<House>();
         _enemySpawnpoint = FindAnyObjectByType<EnemySpawnpoint>();
-        Time.timeScale = 1;
-        _PistleTimer =PistleInterval;
+        Time.timeScale = 1;//再読み込みした時に時間が動くように
+        _PistleTimer =PistleInterval;//
         _ARTimer = ARInterval;
         _SMGTimer = SMGInterval;
         _SMGTimer = SRInterval;
@@ -75,54 +75,61 @@ public class Hero : MonoBehaviour
     void Update()
     {
         if (!Input.GetKey(KeyCode.D) || !Input.GetKey(KeyCode.A) || !Input.GetKey(KeyCode.W))
+            //移動用のキーが押されてなかったら
         {
-            _animator.SetBool("Player_anim",false);
+            _animator.SetBool("Player_anim",false);//移動するアニメーション停止
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(H_speed * Time.deltaTime, 0);
-            _animator.SetBool("Player_anim", true);
-            transform.rotation = Quaternion.Euler(0,180,0);
+            _animator.SetBool("Player_anim", true);//移動するアニメーション実行
+            transform.rotation = Quaternion.Euler(0,180,0);//180度回転
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.position -= new Vector3(H_speed * Time.deltaTime, 0);
             _animator.SetBool("Player_anim", true);
-            transform.rotation = Quaternion.Euler(0,0,0);
+            transform.rotation = Quaternion.Euler(0,0,0);//Dキーを押して回転してた時に元に戻す
         }
         if (Input.GetKeyDown(KeyCode.W) && _isGround && Time.timeScale != 0)
         {
-            _animator.SetTrigger("Jump");
+            _animator.SetTrigger("Jump");//ジャンプアニメーション実行
+            //boolに例えるならtriggerは常にtrueなやつ
+            //移動するアニメーションは放置のアニメーションと行ったり来たりするからbool
+            //ジャンプはany stateからしか繋がってない一方通行なので、trigger
             _rig.AddForce(new Vector2(0, 350));
         }
         if (Input.GetKeyDown(KeyCode.K) && Time.timeScale > 0)//武器切り替え
         {
-            _PlayerIndex = (_PlayerIndex + 1) % weaponList.Length;
-            Debug.Log($"{_PlayerIndex}");
+            _bulletIndex = (_bulletIndex + 1) % bulletList.Length;
+            //%で余りを出すことで、0～3をぐるぐる回ってくれる
+            Debug.Log($"{_bulletIndex}");
             WeaponSelecter();
         }
         if (Input.GetKeyDown(KeyCode.L) && Time.timeScale > 0)
         {
-            _PlayerIndex = (_PlayerIndex - 1 + weaponList.Length) % weaponList.Length;
-            Debug.Log($"{_PlayerIndex}");
+            _bulletIndex = (_bulletIndex - 1 + bulletList.Length) % bulletList.Length;
+            //そのまま引き算するとマイナスになってしまうので、
+            //弾の種類の数(_bulletIndex)から-1したあと、
+            //弾のリストの数(bulletList.Length)を足してから剰余残
+            //変数が違うだけでどちらも最大値は同じだから成り立つ
+            Debug.Log($"{_bulletIndex}");
             WeaponSelecter();
         }
         if (Input.GetKey(KeyCode.Space) && Time.timeScale > 0)
+            //スペースを押したときに時間が止まってなかったら
         {
-            bulletshot();
+            bulletshot();//弾を撃つ
         }
         if (_HouseEnter)//ショップを開く
         {
-            GoldText.gameObject.SetActive(true);
-            ShopUI[0].SetActive(true);
-            ShopUI[1].SetActive(true);
-            ShopUI[2].SetActive(true);
-            ShopUI[3].SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Alpha1) && Checkbuy(WeaponGold[0], ref _ARcount))//ショップで武器を購入するためのやつ
+            ShopUI.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Alpha1) && Checkbuy(WeaponGold[0], ref _ARcount))
             {
                 DecreaseGold(WeaponGold[0]);
                 Debug.Log($"{H_Gold}");
                 Buyweapon(WeaponGameSceneUI[0]);
+                //弾を購入する
             }
             if (Input.GetKeyDown(KeyCode.Alpha2) && Checkbuy(WeaponGold[1], ref _SMGcount))
             {
@@ -139,30 +146,31 @@ public class Hero : MonoBehaviour
         }
         else if (!_HouseEnter)//ショップを閉じる
         {
-            GoldText.gameObject.SetActive(false);
-            ShopUI[0].SetActive(false);
-            ShopUI[1].SetActive(false);
-            ShopUI[2].SetActive(false);
-            ShopUI[3].SetActive(false);
+            ShopUI.SetActive(false);
         }
         if (H_HP <= 0 || _house.HouseHP == _house._enemyIn)
+            //プレイヤーのHP、または耐久値が0になったら
         {
             Destroy(this.gameObject);
             GameOverUI.SetActive(true);
             Time.timeScale = 0;
             GameOverWaveText.text = $"{_enemySpawnpoint.waveCount - 1}" + "ウェーブクリア！！";
+            //死んだ1ウェーブ前をクリア数に表示
             _isDead = true;
         }
 
         if (_enemySpawnpoint._isCleared)
+        //クリアしてたら
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 SceneManager.LoadScene("StartScene");
+                //スタートシーンに移行
             }
             if (Input.GetKeyDown(KeyCode.R))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                //再ロード
             }
         }
     }
@@ -171,6 +179,7 @@ public class Hero : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             _isGround = true;
+            //二段ジャンプできないように
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -178,7 +187,6 @@ public class Hero : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             _isGround = false;
-            _animator.SetBool("Player_Jump", true);
         }
     }
     /// <summary>
@@ -186,29 +194,32 @@ public class Hero : MonoBehaviour
     /// </summary>
     public void bulletshot()
     {
-        if (_PlayerIndex == 0 && _PistleTimer < Time.time)//delta.timeを足し続けるよりもtimeで必要な時だけ呼び出した方が軽い
+        if (_bulletIndex == 0 && _PistleTimer < Time.time)
+            //delta.timeを足し続けるよりもtimeで必要な時だけ呼び出した方が軽い
         {
             Instantiate(Bullet[0],muzzle.transform.position,transform.rotation);
             _audio.PlayOneShot(ShotSE[0]);
             Bullet[0].SetActive(true);
-            _PistleTimer = Time.time + PistleInterval;//if文が呼ばれたら、timeにインターバルを足してピストルタイマーに代入
-            　　　　　　　　　　　　　　　　　　　　　//一時的にtimeよりもタイマーの方がインターバル時間分大きくなるため、ちゃんと機能する
+            _PistleTimer = Time.time + PistleInterval;
+            //if文が呼ばれたら、timeにインターバルを足してピストルタイマーに代入
+            //一時的にtimeよりもタイマーの方がインターバル時間分大きくなるため、ちゃんと機能する
         }
-        if (_PlayerIndex == 1 && _ARTimer < Time.time && _ARcount > 0)
+        if (_bulletIndex == 1 && _ARTimer < Time.time && _ARcount > 0)
         {
-            Instantiate(Bullet[1], muzzle.transform.position, transform.rotation);//キャラが向いている方向に弾を発射
+            Instantiate(Bullet[1], muzzle.transform.position, transform.rotation);
+            //キャラが向いている方向に弾を発射
             _audio.PlayOneShot(ShotSE[1]);
             Bullet[1].SetActive(true);
             _ARTimer = Time.time + ARInterval;
         }
-        if (_PlayerIndex == 2 && _SMGTimer < Time.time && _SMGcount > 0)
+        if (_bulletIndex == 2 && _SMGTimer < Time.time && _SMGcount > 0)
         {
             Instantiate(Bullet[2], muzzle.transform.position, transform.rotation);
             _audio.PlayOneShot(ShotSE[2]);
             Bullet[2].SetActive(true);
             _SMGTimer = Time.time + SMGInterval;
         }
-        if (_PlayerIndex == 3 && _SRTimer < Time.time && _SRcount > 0)
+        if (_bulletIndex == 3 && _SRTimer < Time.time && _SRcount > 0)
         {
             Instantiate(Bullet[3], muzzle.transform.position, transform.rotation);
             _audio.PlayOneShot(ShotSE[3]);
@@ -216,10 +227,16 @@ public class Hero : MonoBehaviour
             _SRTimer = Time.time + SRInterval;
         }
     }
+    /// <summary>
+    /// 金設定用メソッド
+    /// </summary>
     private void SetGold()
     { 
         GoldText.text ="所持金: " + H_Gold.ToString();
     }
+    /// <summary>
+    /// HP設定用メソッド
+    /// </summary>
     public void SetHP()
     {
         HPText.text = "残りHP:" + H_HP.ToString();
@@ -230,7 +247,7 @@ public class Hero : MonoBehaviour
     /// <param name="gold"></param>
     /// <param name="keycount">買うためのキーが押された回数</param>
     /// <returns></returns>
-    private bool Checkbuy(int gold, ref int keycount)
+    public bool Checkbuy(int gold, ref int keycount)
     {
         if (H_Gold >= gold && keycount == 0)
         {
@@ -244,7 +261,7 @@ public class Hero : MonoBehaviour
             return false;
         }
         else if (keycount > 0)
-        {
+        {//1回買われてたら買えないようにする
             WarningUI[1].SetActive(true);
             StartCoroutine(UIfadeout(WarningUI[1]));
             return false;
@@ -287,14 +304,14 @@ public class Hero : MonoBehaviour
     /// </summary>
     public void WeaponSelecter()
     {
-        if (_PlayerIndex == 0)
+        if (_bulletIndex == 0)
         {
             WeaponSelectUI[0].SetActive(true);
             WeaponSelectUI[1].SetActive(false);
             WeaponSelectUI[2].SetActive(false);
             WeaponSelectUI[3].SetActive(false);
         }
-        if (_PlayerIndex == 1)
+        if (_bulletIndex == 1)
         {
             WeaponSelectUI[0].SetActive(false);
             WeaponSelectUI[1].SetActive(true);
@@ -306,7 +323,7 @@ public class Hero : MonoBehaviour
                 StartCoroutine(UIfadeout(WarningUI[2]));
             }
         }
-        if (_PlayerIndex == 2)
+        if (_bulletIndex == 2)
         {
             WeaponSelectUI[0].SetActive(false);
             WeaponSelectUI[1].SetActive(false);
@@ -318,7 +335,7 @@ public class Hero : MonoBehaviour
                 StartCoroutine(UIfadeout(WarningUI[2]));
             }
         }
-        if (_PlayerIndex == 3)
+        if (_bulletIndex == 3)
         {
             WeaponSelectUI[0].SetActive(false);
             WeaponSelectUI[1].SetActive(false);
@@ -339,6 +356,7 @@ public class Hero : MonoBehaviour
     IEnumerator UIfadeout(GameObject fadeoutUI)
     {
         yield return new WaitForSecondsRealtime(2);
+        //時間が止まってるときに出すからRealtimeで動かす
         fadeoutUI.SetActive(false);
     }
 }
