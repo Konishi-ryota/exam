79cdbb3ba7cfacaf1644dd864f83224e3998ed09
ryotakonishi <ryota.konishi@icloud.com>
2025-06-26 03:28:34 +0900

    [add]コメント追加

--- Assets/HealItem.cs
+++ Assets/HealItem.cs
@@ -4,6 +4,7 @@ public class HealItem : MonoBehaviour
 {
     [SerializeField] int HouseHealPower;
     [SerializeField] int HeroHealPower;
+    [SerializeField] int _beforeRemove = 10;
 
     private House _house;
     private Hero _hero;
@@ -12,22 +13,19 @@ public class HealItem : MonoBehaviour
     {
         _house = FindAnyObjectByType<House>();
         _hero = FindAnyObjectByType<Hero>();
+        Destroy(gameObject, _beforeRemove);//n秒後に消す
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)//isTriggerしてるのでTriggerEnter
     {
-        Destroy(gameObject,10);
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")//プレイヤーと当たった時
+            //タグ使ってるコードを先輩に見せると大体「タグ使ってんのか...」って言われる
+            //他にどんなやり方があるのかは調べ得そう、このやり方タイピングミスって死ぬ
         {
             _house.HouseHP += HouseHealPower;
-            _hero.H_HP += HeroHealPower;
+            _hero.H_HP += HeroHealPower;//プレイヤーと家のHPを増やす
             Destroy(this.gameObject);
             _house.SetHouseHP();
-            _hero.SetHP();
+            _hero.SetHP();//それぞれの体力管理メソッド呼び出し
             Debug.Log($"{_house.HouseHP}");
         }
     }
