171987b96ba7e72fd40ec82c398edc742bfe490f
ryotakonishi <ryota.konishi@icloud.com>
2025-06-25 10:00:24 +0900

    [add]ゲームクリア画面追加(一旦)

--- Assets/EnemySpawnpoint.cs
+++ Assets/EnemySpawnpoint.cs
@@ -1,5 +1,5 @@
 using System;
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
@@ -14,20 +14,20 @@ public class EnemySpawnpoint : MonoBehaviour
     private int _secondSpawnrnd;
     private float _timer = 0;
     [NonSerialized] public int _remainTime;
-    [SerializeField] private int waveCount = 1;
-    private int RemainWaveCount;
-    private int MaxWaveCount = StartScene.Maxwave;
+    public int waveCount = 1;
+    public int MaxWaveCount = StartScene.Maxwave;
     public int _stageTimer = 30;
     [SerializeField] Text timerText;
     [SerializeField] Text WaveText;
+    [SerializeField] GameObject GameClearUI;
     private Hero _hero;
-    private int _enemySpawnFrequency;
 
     // Start is called before the first frame update
     void Start()
     {
         _hero = FindAnyObjectByType<Hero>();
         Debug.Log($"{MaxWaveCount}");
+        Debug.Log($"{Application.targetFrameRate}");
         WaveText.text = $"今は{waveCount}ウェーブ目"; 
     }
 
@@ -52,15 +52,16 @@ public class EnemySpawnpoint : MonoBehaviour
     {
         if (waveCount == 1)
         {
-            if (Time.frameCount % (Application.targetFrameRate * 2) == 0)
+            if (Time.frameCount % (Application.targetFrameRate *5) == 0)
             {
                 Debug.Log("敵生成");
+                Debug.Log($"{Time.frameCount}");
                 Instantiate(enemy[0], spawnpoint[0].transform.position, Quaternion.identity);
             }
         }
         if (waveCount == 2)
         {
-            if (Time.frameCount % (Application.targetFrameRate * 2) == 0)
+            if (Time.frameCount % (Application.targetFrameRate * 5) == 0)
             {
                 Debug.Log("敵生成");
                 Instantiate(enemy[2], spawnpoint[1].transform.position, Quaternion.identity);
@@ -68,57 +69,41 @@ public class EnemySpawnpoint : MonoBehaviour
         }
         if (waveCount == 3)
         {
-            if (Time.frameCount % (Application.targetFrameRate * 2) == 0)
+            if (Time.frameCount % (Application.targetFrameRate * 5) == 0)
             {
                 Instantiate(enemy[0], spawnpoint[0].transform.position, Quaternion.identity);
             }
-            if (Time.frameCount %  (Application.targetFrameRate * 2) == 0)
+            if (Time.frameCount %  (Application.targetFrameRate * 5) == 0)
             {
                 Instantiate(enemy[2], spawnpoint[1].transform.position, Quaternion.identity);
             }
         }
         if (waveCount >= 4 && waveCount != MaxWaveCount)
         {
+            if (Time.frameCount % (Application.targetFrameRate * 2) == 0)
+            {
+                _spawnrnd = Random.Range(0, 2);
+                Instantiate(enemy[_spawnrnd], spawnpoint[0].transform.position, Quaternion.identity);
+            }
+            if (Time.frameCount % (Application.targetFrameRate * 2.5) == 0)
+            {
+                _secondSpawnrnd = Random.Range(2, 4);
+                Instantiate(enemy[_secondSpawnrnd], spawnpoint[1].transform.position, Quaternion.identity);
+            }
+        }
+        if (MaxWaveCount == waveCount && MaxWaveCount == 7)
+        {
             if (Time.frameCount % Application.targetFrameRate == 0)
             {
                 _spawnrnd = Random.Range(0, 2);
                 Instantiate(enemy[_spawnrnd], spawnpoint[0].transform.position, Quaternion.identity);
             }
-            if (Time.frameCount % (Application.targetFrameRate + 30) == 0)
+            if (Time.frameCount % (Application.targetFrameRate * 2) == 0)
             {
-                _secondSpawnrnd = Random.Range(0, 3);
-                for (int i = 0; i < _spawnrnd; i++)
-                {
-                    Instantiate(enemy[_secondSpawnrnd], spawnpoint[1].transform.position, Quaternion.identity);
-                }
+                _secondSpawnrnd = Random.Range(2, 4);
+                Instantiate(enemy[_secondSpawnrnd], spawnpoint[1].transform.position, Quaternion.identity);
             }
         }
-        if (MaxWaveCount == waveCount && MaxWaveCount == 7)
-        {
-
-        }
-        //if(waveCount == 1)
-        //{
-        //    _enemySpawnFrequency = Random.Range(2,5);
-        //    Debug.Log($"{_enemySpawnFrequency}");
-        //    if (Time.frameCount % (Application.targetFrameRate * _enemySpawnFrequency) == 0)
-        //    {
-        //      Instantiate(enemy[0], spawnpoint[0].transform.position, Quaternion.identity);
-        //    }
-        //}
-        //if (waveCount == 2)
-        //{
-        //    _enemySpawnFrequency= Random.Range(2,4);
-        //    Debug.Log($"{_enemySpawnFrequency}");
-        //    if (Time.frameCount % (Application.targetFrameRate * _enemySpawnFrequency) == 0)
-        //    {
-        //        Instantiate(enemy[0], spawnpoint[0].transform.position,Quaternion.identity);
-        //    }
-        //}
-        //if (waveCount == 3)
-        //{
-
-        //}
     }
     /// <summary>
     /// ウェーブ切り替え機能
@@ -128,7 +113,7 @@ public class EnemySpawnpoint : MonoBehaviour
         _timer += Time.deltaTime;
         _remainTime = _stageTimer - (int)_timer;
         timerText.text = "残り時間 " + _remainTime.ToString("D2") + " 秒";
-        if (_remainTime <= 0)
+        if (_remainTime <= 0 && waveCount != MaxWaveCount)
         {
             _hero._HouseEnter = true;
             _timer = _stageTimer;//Sを押すまでは残り時間を0秒にする
@@ -149,8 +134,15 @@ public class EnemySpawnpoint : MonoBehaviour
                 if (waveCount == MaxWaveCount)
                 {
                     WaveText.text = "今は最終ウェーブ";
+                    StartCoroutine(EndGame());
                 }
             }
         }
+    }
+    IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(10);
+        GameClearUI.SetActive(true);
+        Time.timeScale = 0;
     }
 }
