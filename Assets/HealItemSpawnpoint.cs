2b3e1f532f5d29852dfa58d9e2734fcba3e598b5
ryotakonishi <ryota.konishi@icloud.com>
2025-06-26 03:28:46 +0900

    [add]コメント追加

--- Assets/HealItemSpawnpoint.cs
+++ Assets/HealItemSpawnpoint.cs
@@ -17,11 +17,15 @@ public class HealItemSpawnpoint : MonoBehaviour
     void Update()
     {
         if (_enemySpawnpoint.waveCount == 3 || _enemySpawnpoint.waveCount == 5 || _enemySpawnpoint.waveCount == 7)
+        //ウェーブ数が3または、5、または7の時
         {
             CreateItem();
             StartCoroutine(Spawnbool());
         }
     }
+    /// <summary>
+    /// アイテム生成用メソッド
+    /// </summary>
     private void CreateItem()
     {
         if (!_isSpawned && Time.timeScale != 0)
@@ -31,9 +35,15 @@ public class HealItemSpawnpoint : MonoBehaviour
             Instantiate(HealItem, transform.position, Quaternion.identity);
         }
     }
+    /// <summary>
+    /// 1ウェーブに1個生成する用
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Spawnbool()
     {
         yield return new WaitForSeconds(_enemySpawnpoint._stageTimer);
         _isSpawned = false;
+        //これも1回動かしたら終わらせるやり方がわからんかったからごり押し
+        //Startでも使うんだろうか、でもそういう1回だけって意味じゃないからなぁ
     }
 }
